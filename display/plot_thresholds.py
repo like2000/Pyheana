5c2da201f52933fb8a1ac7f3f78a8853758e8d16
michuschenk <michuschenk@gmail.com>
2014-07-10 15:07:20 +0200

    Changes plot_thresholds from class structure back to 'collection of methods'. Simpler.

--- display/plot_thresholds.py
+++ display/plot_thresholds.py
@@ -1,8 +1,7 @@
 '''
-@class ThresholdPlots
 @author Kevin Li, Michael Schenk
-@date 07.07.14 (last update)
-@Class for creation of threshold plots from bunch data.
+@date 10.07.14 (last update)
+@Collection of methods for the creation of threshold plots from bunch data.
 @copyright CERN
 '''
 import matplotlib
@@ -11,68 +10,54 @@ import scipy.io as sio
 from Pyheana.load.load_data import *
 
 
-class ThresholdPlots(object):
+def plot_thresholds(rawdata, scan_values, plane='horizontal',
+                    xlabel='turns', ylabel='intensity [particles]', zlabel='normalized emittance',
+                    xlimits=((0.,8192)), ylimits=((0.,7.1e11)), zlimits=((0., 10.))):
 
-    def __init__(self, rawdata, scan_values):
-
-        self.scan_values = scan_values
-        self._prepare_input_signals(rawdata)
+    # Prepare input data.
+    # x axis
+    t = rawdata[0,:,:]
+    turns = plt.ones(t.shape).T * plt.arange(len(t))
+    turns = turns.T
         
+    # z axis
+    epsn_abs = {}
+    epsn_abs['horizontal'] = plt.absolute(rawdata[11,:,:])
+    epsn_abs['vertical']   = plt.absolute(rawdata[12,:,:])
+
+    # Prepare plot environment.
+    ax11, ax13 = _create_axes(xlabel, ylabel, zlabel, xlimits, ylimits, zlimits)
+    cmap = plt.cm.get_cmap('jet', 2)
+    ax11.patch.set_facecolor(cmap(range(2))[-1])
+    cmap = plt.cm.get_cmap('jet')
+
+    x, y = plt.meshgrid(turns[:,0], scan_values)
+    z = epsn_abs[plane]
+
+    threshold_plot = ax11.contourf(x, y, z.T, levels=plt.linspace(zlimits[0], zlimits[1], 201),
+                                   vmin=zlimits[0], vmax=zlimits[1], cmap=cmap)
+    cb = plt.colorbar(threshold_plot, ax13, orientation='vertical')
+    cb.set_label(zlabel)
+
+    plt.tight_layout()
+    
+
+def _create_axes(xlabel, ylabel, zlabel, xlimits, ylimits, zlimits):
+
+    # Plot environment
+    ratio = 20
+    fig = plt.figure(figsize=(22, 12))
         
-    def prepare_plot(self, plane='horizontal', xlabel='turns', ylabel='intensity [particles]', zlabel='normalized emittance',
-                     xlimits=((0.,8192)), ylimits=((0.,7.1e11)), zlimits=((0., 10.))):
+    gs = matplotlib.gridspec.GridSpec(1, ratio)
+    ax11 = plt.subplot(gs[0,:ratio-1])
+    ax13 = plt.subplot(gs[:,ratio-1])
 
-        if not plane in ('horizontal', 'vertical'):
-            print 'Invalid plane argument. Must be horizontal or vertical.'
-            exit(-1)
+    # Prepare plot axes
+    ax11.grid(color='w')
+    ax11.set_axis_bgcolor('0.35')
+    ax11.set_xlabel(xlabel);
+    ax11.set_xlim(xlimits)
+    ax11.set_ylabel(ylabel)
+    ax11.set_ylim(ylimits)
         
-        self._create_axes(xlabel, ylabel, zlabel, xlimits, ylimits, zlimits)
-        
-        cmap = plt.cm.get_cmap('jet', 2)
-        self.ax11.patch.set_facecolor(cmap(range(2))[-1])
-        cmap = plt.cm.get_cmap('jet')
-
-        x, y = plt.meshgrid(self.turns[:,0], self.scan_values)
-        z = self.epsn_abs[plane]
-        threshold_plot = self.ax11.contourf(x, y, z.T, levels=plt.linspace(zlimits[0], zlimits[1], 201),
-                                            vmin=zlimits[0], vmax=zlimits[1], cmap=cmap)
-        cb = plt.colorbar(threshold_plot, self.ax13, orientation='vertical')
-        cb.set_label(zlabel)
-
-        plt.tight_layout()
-
-        
-    def _prepare_input_signals(self, rawdata):
-
-        # x axis
-        t = rawdata[0,:,:]
-        turns = plt.ones(t.shape).T * plt.arange(len(t))
-        turns = turns.T
-        self.turns = turns
-        
-        # z axis
-        self.epsn_abs = {}
-        self.epsn_abs['horizontal'] = plt.absolute(rawdata[11,:,:])
-        self.epsn_abs['vertical']   = plt.absolute(rawdata[12,:,:])
-            
-
-    def _create_axes(self, xlabel, ylabel, zlabel, xlimits, ylimits, zlimits):
-
-        # Plot environment
-        ratio = 20
-        fig = plt.figure(figsize=(22, 12))
-        
-        gs = matplotlib.gridspec.GridSpec(1, ratio)
-        ax11 = plt.subplot(gs[0,:ratio-1])
-        ax13 = plt.subplot(gs[:,ratio-1])
-
-        # Prepare plot axes
-        ax11.grid(color='w')
-        ax11.set_axis_bgcolor('0.35')
-        ax11.set_xlabel(xlabel);
-        ax11.set_xlim(xlimits)
-        ax11.set_ylabel(ylabel)
-        ax11.set_ylim(ylimits)
-
-        self.ax11 = ax11
-        self.ax13 = ax13
+    return ax11, ax13
--- examples/threshold_plots/threshold_plots.py
+++ examples/threshold_plots/threshold_plots.py
@@ -5,21 +5,20 @@ import numpy as np
 import MyStyle as ms
 ms.set_mystyle()
 
-
 # INPUT SETTINGS
 scan_parameter  = 'intensity'  # Use 'intensity', or 'i_oct_d' (octupole current sweep), or ...
 
-input_path      = '/data/TECH/Experiments/LHC/4TeV/PyHT/data/run_LHC_4TeV_iSwp_O0_BBResonator_newTracker/'
-file_list       = get_file_list(input_path, '*.h5')
-data, file_list = read_bunch_data(file_list[:], format='h5')
-L               = get_attributes(file_list, [scan_parameter], format='h5')
+input_path         = '/data/TECH/Experiments/LHC/4TeV/PyHT/data/run_LHC_4TeV_iSwp_O0_BBResonator_newTracker/'
+file_list          = get_file_list(input_path, '*.h5')
+rawdata, file_list = read_bunch_data(file_list[:], format='h5')
+L                  = get_attributes(file_list, [scan_parameter], format='h5')
 
 # OUTPUT SETTINGS
 output_path     = './'
 output_filename = 'thresholds_iSwp_O0_BBResonatorDipX_PyHT.png'
 
 # Plot parameters.
-plane   = 'horizontal'
+plane   = 'vertical' # Use 'horizontal' or 'vertical'.
 xlabel  = 'turns'
 ylabel  = 'intensity [particles]'
 zlabel  = plane + ' normalized emittance [$\mu m$]' 
@@ -28,8 +27,6 @@ ylimits = ((0., 8.6e11))
 zlimits = ((0., 10.))    
 
 # CREATE PLOT, SAVE AND SHOW.
-threshold_plots = ThresholdPlots(rawdata=data, scan_values=L[scan_parameter])
-
-threshold_plots.prepare_plot(plane, xlabel, ylabel, zlabel, xlimits, ylimits, zlimits)
+plot_thresholds(rawdata, L[scan_parameter], plane, xlabel, ylabel, zlabel, xlimits, ylimits, zlimits)
 plt.savefig(output_path + plane + '_' + output_filename, dpi=300)
 plt.show()
