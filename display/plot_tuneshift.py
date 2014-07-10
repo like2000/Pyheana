c1762a403c3fc86b87b8ace3354e5e04c5f47b2f
michuschenk <michuschenk@gmail.com>
2014-07-10 14:50:08 +0200

    Changes from class structure back to 'collection of methods'. Simpler.

--- display/plot_tuneshift.py
+++ display/plot_tuneshift.py
@@ -1,8 +1,7 @@
 '''
-@class TuneshiftPlots
 @author Kevin Li, Michael Schenk
-@date 07.07.14 (last update)
-@Class for creation of tuneshift plots from bunch data.
+@date 10.07.14 (last update)
+@brief Collection of methods to generate tuneshift plots.
 @copyright CERN
 '''
 import PySussix
@@ -11,195 +10,181 @@ import pylab as plt
 import scipy.io as sio
 
 
-class TuneshiftPlots(object):
+def calculate_tuneshifts(rawdata, beta_x, beta_y, Q_x, Q_y, Q_s, analyzer='fft', n_lines=None):
 
-    def __init__(self, rawdata, scan_values, beta_x, beta_y, Q_x, Q_y, Q_s):
+    # Prepare signals and perform spectral analysis.
+    sx, sy = _prepare_input_signals(rawdata, beta_x, beta_y)
+    if analyzer == 'fft':
+        spectra = _calculate_spectra_fft(sx, sy, Q_x, Q_y, Q_s, n_lines=1000)
+    elif analyzer == 'sussix':
+        spectra = _calculate_spectra_sussix(sx, sy, Q_x, Q_y, Q_s, n_lines=600)
+    else:
+        print 'Invalid analyzer argument.'
 
-        self.scan_values = scan_values
-        self.Q_x = Q_x
-        self.Q_y = Q_y
-        self.Q_s = Q_s
+    return spectra
 
-        self._prepare_input_signals(rawdata, beta_x, beta_y)
-
-        # Dictionaries to store results of different cases (horizontal, vertical) x (fft, sussix).
-        self.spectral_lines = {}
-        self.spectral_intensity = {}
-
-        
-    def calculate_spectrogram(self, analyzer):
-
-        if analyzer == 'fft':
-            self._calculate_fft_spectrogram()
-        elif analyzer == 'sussix':
-            self._calculate_sussix_spectrogram()
-        else:
-            print 'Invalid analyzer argument.'
-            
-        
-    def prepare_plot(self, plane='horizontal', analyzer='fft', xlabel='intensity [particles]', ylabel='mode number',
-                     xlimits=((0.,7.1e11)), ylimits=((-4,2))):
-
-        if not plane in ('horizontal', 'vertical'):
-            raise ValueError, 'Invalid plane argument.'
-
-        if not analyzer in ('fft', 'sussix'):
-            raise ValueError, 'Invalid analyzer argument.'
-
-        if not self.spectral_lines.has_key((analyzer,plane)):
-            self.calculate_spectrogram(analyzer)
-
-        # Set up plot environment.
-        self._create_axes(xlabel, ylabel, xlimits, ylimits)
-        self._create_cropped_cmap()
-        
-        # Normalize power.
-        norm_intensity = self.spectral_intensity[(analyzer,plane)] / plt.amax(self.spectral_intensity[(analyzer,plane)])
-        spec_lines = self.spectral_lines[(analyzer,plane)]
-        
-        x_grid = plt.ones(spec_lines.shape) * plt.array(self.scan_values, dtype='float64')
-        for file_i in xrange(self.n_files):
-            x, y, z = x_grid[:,file_i], spec_lines[:,file_i], norm_intensity[:,file_i]
-            tuneshift_plot = self.ax11.scatter(x, y, s=192*plt.log(1+z), c=z, cmap=self.palette, edgecolors='None')
-
-        # Colorbar
-        cb = plt.colorbar(tuneshift_plot, self.ax13, orientation='vertical')
-        cb.set_label('Power [normalised]')
-                
-        plt.tight_layout()
-
-                
-    def _prepare_input_signals(self, rawdata, beta_x, beta_y):
-
-        n_variables, n_turns, n_files = rawdata.shape
-        
-        sx = plt.zeros((n_turns, n_files), dtype='complex')
-        sy = plt.zeros((n_turns, n_files), dtype='complex')
-
-        for file_i in range(n_files):
-            x, xp, y, yp = rawdata[1:5,:,file_i]
-
-            # Floquet transformation
-            xp *= beta_x[file_i]
-            yp *= beta_y[file_i]
-
-            # Signal
-            sx[:,file_i] = x - 1j * xp
-            sy[:,file_i] = y - 1j * yp
-
-        self.sx = sx
-        self.sy = sy
-        self.n_turns = n_turns
-        self.n_files = n_files
-
-
-    def _calculate_fft_spectrogram(self, n_lines=1000):
-
-        # Allocate memory for output.
-        oxx, axx = plt.zeros((n_lines, self.n_files)), plt.zeros((n_lines, self.n_files))
-        oyy, ayy = plt.zeros((n_lines, self.n_files)), plt.zeros((n_lines, self.n_files))
-
-        for file_i in xrange(self.n_files):
-            t = plt.linspace(0, 1, self.n_turns)
-            ax = plt.absolute(plt.fft(self.sx[:, file_i]))
-            ay = plt.absolute(plt.fft(self.sy[:, file_i]))
-
-            # Amplitude normalisation
-            ax /= plt.amax(ax, axis=0)
-            ay /= plt.amax(ay, axis=0)
-
-            # Tunes
-            if file_i==0:
-                tunexfft = t[plt.argmax(ax[:self.n_turns/2], axis=0)]
-                tuneyfft = t[plt.argmax(ay[:self.n_turns/2], axis=0)]
-                print "\n*** Tunes from FFT"
-                print "    tunex:", tunexfft, ", tuney:", tuneyfft, "\n"
-
-            # Tune normalisation
-            ox = (t - (self.Q_x[file_i] % 1)) / self.Q_s[file_i]
-            oy = (t - (self.Q_y[file_i] % 1)) / self.Q_s[file_i]
     
-            # Sort
-            CX = plt.rec.fromarrays([ox, ax], names='ox, ax')
-            CX.sort(order='ax')
-            CY = plt.rec.fromarrays([oy, ay], names='oy, ay')
-            CY.sort(order='ay')
-            ox, ax, oy, ay = CX.ox[-n_lines:], CX.ax[-n_lines:], CY.oy[-n_lines:], CY.ay[-n_lines:]
-            oxx[:,file_i], axx[:,file_i], oyy[:,file_i], ayy[:,file_i] = ox, ax, oy, ay
+def plot_tuneshifts(spectrum, scan_values, xlabel='intensity [particles]', ylabel='mode number',
+                    xlimits=((0.,7.1e11)), ylimits=((-4,2))):
 
-        self.spectral_lines.update({ ("fft", "horizontal"): oxx, ("fft", "vertical"): oyy })
-        self.spectral_intensity.update({ ("fft", "horizontal"): axx, ("fft", "vertical"): ayy })
+    (spectral_lines, spectral_intensity) = spectrum
 
+    # Normalize power.
+    normalized_intensity = spectral_intensity / plt.amax(spectral_intensity)
 
-    def _calculate_sussix_spectrogram(self, n_lines=600):
-
-        # Initialise Sussix object.
-        SX = PySussix.Sussix()
-
-        # Allocate memory for output.        
-        oxx, axx = plt.zeros((n_lines, self.n_files)), plt.zeros((n_lines, self.n_files))
-        oyy, ayy = plt.zeros((n_lines, self.n_files)), plt.zeros((n_lines, self.n_files))
-
-        x, xp, y, yp = self.sx.real, self.sx.imag, self.sy.real, self.sy.imag
-        for file_i in xrange(self.n_files):
-            SX.sussix_inp(nt1=1, nt2=self.n_turns, idam=2, ir=0, tunex=self.Q_x[file_i] % 1, tuney=self.Q_y[file_i] % 1)
-            SX.sussix(x[:,file_i], xp[:,file_i], y[:,file_i], yp[:,file_i], self.sx[:,file_i], self.sx[:,file_i])
-
-            # Amplitude normalisation
-            SX.ax /= plt.amax(SX.ax)
-            SX.ay /= plt.amax(SX.ay)
-
-            # Tunes
-            SX.ox = plt.absolute(SX.ox)
-            SX.oy = plt.absolute(SX.oy)
-            if file_i==0:
-                tunexsx = SX.ox[plt.argmax(SX.ax)]
-                tuneysx = SX.oy[plt.argmax(SX.ay)]
-                print "\n*** Tunes from Sussix"
-                print "    tunex", tunexsx, ", tuney", tuneysx, "\n"
-
-            # Tune normalisation
-            SX.ox = (SX.ox - (self.Q_x[file_i] % 1)) / self.Q_s[file_i]
-            SX.oy = (SX.oy - (self.Q_y[file_i] % 1)) / self.Q_s[file_i]
+    # Prepare plot environment.
+    ax11, ax13 = _create_axes(xlabel, ylabel, xlimits, ylimits)
+    palette    = _create_cropped_cmap()
     
-            # Sort
-            CX = plt.rec.fromarrays([SX.ox, SX.ax], names='ox, ax')
-            CX.sort(order='ax')
-            CY = plt.rec.fromarrays([SX.oy, SX.ay], names='oy, ay')
-            CY.sort(order='ay')
-            ox, ax, oy, ay = CX.ox, CX.ax, CY.oy, CY.ay
-            oxx[:,file_i], axx[:,file_i], oyy[:,file_i], ayy[:,file_i] = ox, ax, oy, ay
+    x_grid = plt.ones(spectral_lines.shape) * plt.array(scan_values, dtype='float64')
+    for file_i in xrange(len(scan_values)):
+        x, y, z = x_grid[:,file_i], spectral_lines[:,file_i], normalized_intensity[:,file_i]
+        tuneshift_plot = ax11.scatter(x, y, s=192*plt.log(1+z), c=z, cmap=palette, edgecolors='None')
 
-        self.spectral_lines.update({ ("sussix", "horizontal"): oxx, ("sussix", "vertical"): oyy })
-        self.spectral_intensity.update({ ("sussix", "horizontal"): axx, ("sussix", "vertical"): ayy })
+    # Colorbar
+    cb = plt.colorbar(tuneshift_plot, ax13, orientation='vertical')
+    cb.set_label('Power [normalised]')
 
+    plt.tight_layout()
         
-    def _create_axes(self, xlabel, ylabel, xlimits, ylimits):
-
-        # Plot environment
-        ratio = 20
-        fig = plt.figure(figsize=(22, 12))
         
-        gs = matplotlib.gridspec.GridSpec(1, ratio)
-        ax11 = plt.subplot(gs[0,:ratio-1])
-        ax13 = plt.subplot(gs[:,ratio-1])
+def _prepare_input_signals(rawdata, beta_x, beta_y):
 
-        # Prepare plot axes
-        ax11.grid(color='w')
-        ax11.set_axis_bgcolor('0.35')
-        ax11.set_xlabel(xlabel);
-        ax11.set_xlim(xlimits)
-        ax11.set_ylabel(ylabel)
-        ax11.set_ylim(ylimits)
+    n_variables, n_turns, n_files = rawdata.shape
 
-        self.ax11 = ax11
-        self.ax13 = ax13
+    sx = plt.zeros((n_turns, n_files), dtype='complex')
+    sy = plt.zeros((n_turns, n_files), dtype='complex')
 
+    for file_i in range(n_files):
+        x, xp, y, yp = rawdata[1:5,:,file_i]
+
+        # Floquet transformation
+        xp *= beta_x[file_i]
+        yp *= beta_y[file_i]
+
+        # Signal
+        sx[:,file_i] = x - 1j * xp
+        sy[:,file_i] = y - 1j * yp
+
+    return sx, sy
+
+
+def _create_axes(xlabel, ylabel, xlimits, ylimits):
+
+    # Plot environment
+    ratio = 20
+    fig = plt.figure(figsize=(22, 12))
+        
+    gs = matplotlib.gridspec.GridSpec(1, ratio)
+    ax11 = plt.subplot(gs[0,:ratio-1])
+    ax13 = plt.subplot(gs[:,ratio-1])
+
+    # Prepare plot axes
+    ax11.grid(color='w')
+    ax11.set_axis_bgcolor('0.35')
+    ax11.set_xlabel(xlabel);
+    ax11.set_xlim(xlimits)
+    ax11.set_ylabel(ylabel)
+    ax11.set_ylim(ylimits)
+
+    return ax11, ax13
+        
                 
-    def _create_cropped_cmap(self, n_segments = 12, cmap='hot'):
+def _create_cropped_cmap(n_segments = 12, cmap='hot'):
 
-        new_cmap = plt.cm.get_cmap(cmap, n_segments)(range(n_segments))
-        new_cmap = plt.delete(new_cmap, (0, -1), 0)
-        palette = matplotlib.colors.LinearSegmentedColormap.from_list("palette", new_cmap)
+    new_cmap = plt.cm.get_cmap(cmap, n_segments)(range(n_segments))
+    new_cmap = plt.delete(new_cmap, (0, -1), 0)
+    palette = matplotlib.colors.LinearSegmentedColormap.from_list("palette", new_cmap)
+        
+    return palette
 
-        self.palette = palette
+
+def _calculate_spectra_fft(sx, sy, Q_x, Q_y, Q_s, n_lines):
+
+    n_turns, n_files = sx.shape
+        
+    # Allocate memory for output.
+    oxx, axx = plt.zeros((n_lines, n_files)), plt.zeros((n_lines, n_files))
+    oyy, ayy = plt.zeros((n_lines, n_files)), plt.zeros((n_lines, n_files))
+
+    for file_i in xrange(n_files):
+        t = plt.linspace(0, 1, n_turns)
+        ax = plt.absolute(plt.fft(sx[:, file_i]))
+        ay = plt.absolute(plt.fft(sy[:, file_i]))
+
+        # Amplitude normalisation
+        ax /= plt.amax(ax, axis=0)
+        ay /= plt.amax(ay, axis=0)
+    
+        # Tunes
+        if file_i==0:
+            tunexfft = t[plt.argmax(ax[:n_turns/2], axis=0)]
+            tuneyfft = t[plt.argmax(ay[:n_turns/2], axis=0)]
+            print "\n*** Tunes from FFT"
+            print "    tunex:", tunexfft, ", tuney:", tuneyfft, "\n"
+
+        # Tune normalisation
+        ox = (t - (Q_x[file_i] % 1)) / Q_s[file_i]
+        oy = (t - (Q_y[file_i] % 1)) / Q_s[file_i]
+    
+        # Sort
+        CX = plt.rec.fromarrays([ox, ax], names='ox, ax')
+        CX.sort(order='ax')
+        CY = plt.rec.fromarrays([oy, ay], names='oy, ay')
+        CY.sort(order='ay')
+        ox, ax, oy, ay = CX.ox[-n_lines:], CX.ax[-n_lines:], CY.oy[-n_lines:], CY.ay[-n_lines:]
+        oxx[:,file_i], axx[:,file_i], oyy[:,file_i], ayy[:,file_i] = ox, ax, oy, ay
+
+    spectra = {}
+    spectra['horizontal'] = (oxx, axx)
+    spectra['vertical']   = (oyy, ayy)
+        
+    return spectra
+    
+
+def _calculate_spectra_sussix(sx, sy, Q_x, Q_y, Q_s, n_lines):
+
+    n_turns, n_files = sx.shape
+
+    # Allocate memory for output.        
+    oxx, axx = plt.zeros((n_lines, n_files)), plt.zeros((n_lines, n_files))
+    oyy, ayy = plt.zeros((n_lines, n_files)), plt.zeros((n_lines, n_files))
+
+    # Initialise Sussix object.
+    SX = PySussix.Sussix()
+    
+    x, xp, y, yp = sx.real, sx.imag, sy.real, sy.imag
+    for file_i in xrange(n_files):
+        SX.sussix_inp(nt1=1, nt2=n_turns, idam=2, ir=0, tunex=Q_x[file_i] % 1, tuney=Q_y[file_i] % 1)
+        SX.sussix(x[:,file_i], xp[:,file_i], y[:,file_i], yp[:,file_i], sx[:,file_i], sx[:,file_i])
+
+        # Amplitude normalisation
+        SX.ax /= plt.amax(SX.ax)
+        SX.ay /= plt.amax(SX.ay)
+
+        # Tunes
+        SX.ox = plt.absolute(SX.ox)
+        SX.oy = plt.absolute(SX.oy)
+        if file_i==0:
+            tunexsx = SX.ox[plt.argmax(SX.ax)]
+            tuneysx = SX.oy[plt.argmax(SX.ay)]
+            print "\n*** Tunes from Sussix"
+            print "    tunex", tunexsx, ", tuney", tuneysx, "\n"
+
+        # Tune normalisation
+        SX.ox = (SX.ox - (Q_x[file_i] % 1)) / Q_s[file_i]
+        SX.oy = (SX.oy - (Q_y[file_i] % 1)) / Q_s[file_i]
+    
+        # Sort
+        CX = plt.rec.fromarrays([SX.ox, SX.ax], names='ox, ax')
+        CX.sort(order='ax')
+        CY = plt.rec.fromarrays([SX.oy, SX.ay], names='oy, ay')
+        CY.sort(order='ay')
+        ox, ax, oy, ay = CX.ox, CX.ax, CY.oy, CY.ay
+        oxx[:,file_i], axx[:,file_i], oyy[:,file_i], ayy[:,file_i] = ox, ax, oy, ay
+
+    spectra = {}
+    spectra['horizontal'] = (oxx, axx)
+    spectra['vertical']   = (oyy, ayy)
+        
+    return spectra
--- examples/tuneshift_plots/tuneshift_plots.py
+++ examples/tuneshift_plots/tuneshift_plots.py
@@ -7,41 +7,40 @@ ms.set_mystyle()
 
 
 # Directory containing bunch (or slice) monitor input files of the (intensity) sweep.
-input_path = '/data/TECH/Experiments/LHC/4TeV/PyHT/data/run_LHC_4TeV_iSwp_O0_BBResonator_newTracker/'
-
+input_path = '/data/TECH/Experiments/LHC/4TeV/PyHT/data/run_LHC_4TeV_iSwp_O0_newTracker_noSigmaz_NSL250/'
 
 # SETTINGS
-analyzer        = 'sussix'     # Use 'sussix' or 'fft'.
+analyzer        = 'sussix'        # Use 'sussix' or 'fft'.
 scan_parameter  = 'intensity'  # Use 'intensity' (intensity sweep), or ...
 output_path     = './'
-output_filename = 'tuneshift_' + analyzer + '_iSwp_O0_BBResonatorDipX_PyHT.png'
-
+output_filename = 'tuneshift_' + analyzer + '_iSwp_O0_noSigmaz_NSL250_PyHT.png'
 
 # READ DATA AND ATTRIBUTES FROM HDF5.
 file_list          = get_file_list(input_path, '*.h5')
 rawdata, file_list = read_bunch_data(file_list[:], format='h5')
-
 L = get_attributes(file_list, ['Q_x', 'Q_y', 'Q_s', 'beta_x', 'beta_y', scan_parameter], format='h5')
 
-
 # CREATE TUNESHIFT PLOTS.
-# Note that calling TuneshiftPlots.calculate_spectrogram(analyzer) calculates and saves data for both planes.
-# Using TuneshiftPlots.prepare_plot(...), the data can be plotted for the plane and analyzer indicated (see below).
-tuneshift_plots = TuneshiftPlots(rawdata, L[scan_parameter], L['beta_x'], L['beta_y'], L['Q_x'], L['Q_y'], L['Q_s'])
-tuneshift_plots.calculate_spectrogram(analyzer=analyzer)
+# Returns a dictionary with keys 'horizontal' and 'vertical' denoting the horizontal and the vertical planes
+# respectively. The values of the dictionary are 2-tuples, consisting of (spectral_lines, spectral_intensity)
+# (data for each value of the scan parameter). 
+spectra = calculate_tuneshifts(rawdata, L['beta_x'], L['beta_y'], L['Q_x'], L['Q_y'], L['Q_s'], analyzer)
 
-# Save and show horizontal plot.
+# Plot and save horizontal spectrogram.
 plane = 'horizontal'
-tuneshift_plots.prepare_plot(plane=plane, analyzer=analyzer,
-                             xlabel='intensity [particles]', ylabel=plane+' mode number',
-                             xlimits=((0., 9.1e11)), ylimits=((-5,3)))
+plot_tuneshifts(spectra[plane], scan_values=L[scan_parameter],
+                xlabel='intensity [particles]', ylabel=plane+' mode number',
+                xlimits=((0., 7.1e11)), ylimits=((-4,2)))
+
 plt.savefig(output_path + plane + '_' + output_filename, dpi=300)
 plt.show()
 
-# Save and show vertical plot.
+
+# Plot and save vertical spectrogram.
 plane = 'vertical'
-tuneshift_plots.prepare_plot(plane=plane, analyzer=analyzer,
-                             xlabel='intensity [particles]', ylabel=plane+' mode number',
-                             xlimits=((0., 9.1e11)), ylimits=((-5,3)))
+plot_tuneshifts(spectra[plane], scan_values=L[scan_parameter],
+                xlabel='intensity [particles]', ylabel=plane+' mode number',
+                xlimits=((0., 7.1e11)), ylimits=((-4,2)))
+
 plt.savefig(output_path + plane + '_' + output_filename, dpi=300)
 plt.show()
